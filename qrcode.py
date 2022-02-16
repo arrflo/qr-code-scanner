d6cbbfaba8102ce0aaea48d335283a6f3fa527a2
romalynfloranda <romalynfloranda0203@gmail.com>
2022-02-16 16:14:55 +0800

    complete program, still untested

--- qrcode.py
+++ qrcode.py
@@ -8,22 +8,48 @@
 #read qr code
 #create a text file
 
+from re import A
 import cv2
 import webbrowser
+from cv2 import line
+from pyzbar.pyzbar import decode
+import datetime
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+
+#variables for cam
+webcam = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
 
 while True:
-    _, img = cap.read()
-    data, _ = detector.detectAndDecode(img)
+    _, img = webcam.read()
+    data, code, _ = detector.detectAndDecode(img)
     if data:
-        a= data
+        rd = data
+        #create a txt file
+        with open ("qrcodescannedresults.txt", 'a') as file:
+            file.write(f'Scanned QR code results: {rd}')
+            file.write('data recorded at' (datetime.datetime.now()))
         break
-    cv2.imshow("QRCODEscanner",img)
-    if cv2.waitKey(1) == ord ("q"):
+    for barcode in decode(img):
+        rd = data
+        txt = barcode.data.decode ('utf-8')
+        print(txt)
+        lne= np.array ([barcode.polygon], np.int32)
+        lne = lne.reshape ((-1,1,2))
+        cv2.polylines(img, [lne], True, (230, 67, 107),5 )
+        with open("qrcodescannedresults.txt", 'a') as file:
+            file.write(f'Scanned QR code results: {rd}')
+            file.write('data recorded at' (datetime.datetime.now()))
+            webcam.release (txt)
+            cv2.destroyAllWindows
+        break
+    cv2.imshow('QR Code Scanner here!', img)
+    if cv2.waitKey(1) == ord('a'):
         break
 
-b = webbrowser.open(str(a))
-cap.release()
-cv2.destroyAllWindows()
+go = webbrowser.open((str(rd)))
+webcam.release (rd)
+cv2.destroyAllWindows
+
+
